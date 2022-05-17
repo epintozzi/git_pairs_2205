fd8c6b50476835ece7511c95dd8af0778033082c
Michael Koulouvaris <mikekoul@gmail.com>
2022-05-17 10:27:51 -0600

    Update bank account

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -17,8 +17,8 @@ describe BankAccount do
     expect(checking.balance).to eq 250.0
   end
 
-  xit '3. can deposit money' do
-    checking = BankAccount.new(123, '0987', 250.0)
+  it '3. can deposit money' do
+    checking = BankAccount.new(123, '0987', 350.0)
     checking.deposit(100.0)
 
     expect(checking.balance).to eq 350.0
